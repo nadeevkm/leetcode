37ac03293840a47fc1c412465d617d76abf53475
nadeevkm <32716706+nadeevkm@users.noreply.github.com>
2025-02-13 12:35:04 +0100

    Update 1448.py

--- algo/repeat_py/1448.py
+++ algo/repeat_py/1448.py
@@ -1,1 +1,20 @@
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
 
+class Solution:
+    def goodNodes(self, root: TreeNode) -> int:
+        def goodNodes(self, root: TreeNode, maxSoFar: int) -> int:
+            if root is None:
+                return 0
+            count = 0
+            if maxSoFar <= root.val:
+                count += 1
+                maxSoFar = root.val
+            count += goodNodes(self, root.left, maxSoFar)
+            count += goodNodes(self, root.right, maxSoFar)
+            return count
+
+        return goodNodes(self, root, root.val)
