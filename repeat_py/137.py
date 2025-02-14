779d6c2bd5172e85b7487055d104f33aaaa46f49
nadeevkm <32716706+nadeevkm@users.noreply.github.com>
2025-02-14 14:19:23 +0100

    Update 137.py

--- repeat_py/137.py
+++ repeat_py/137.py
@@ -1,1 +1,14 @@
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        ans = 0
+        for b in range(32):
+            resBit = 0
+            for num in nums:
+                currBit = (num >> b) & 1
+                resBit += currBit
+            ans += (resBit % 3) << b
+        # check and consider sign-bit in python num representation
+        if ans & 1 << 31:  # i.e. != 0
+            ans -= 2 ** 32
+        return ans
 
