4ea128b874b228e86d45bbc2e72f1acf3db77148
nadeevkm <32716706+nadeevkm@users.noreply.github.com>
2025-02-14 14:19:01 +0100

    Update 47.py

--- repeat_py/47.py
+++ repeat_py/47.py
@@ -1,1 +1,33 @@
+from typing import List
 
+
+class Solution:
+    def permuteUnique(self, nums: List[int]) -> List[List[int]]:
+        res: List[List[int]] = list()
+
+        counts = dict()
+        for n in nums:
+            if n not in counts:
+                counts[n] = 1
+            else:
+                counts[n] += 1
+
+        def permRec(counts, current):
+            if len(current) == len(nums):
+                res.append(current.copy())
+                return
+
+            for val in counts:
+                if counts[val]:
+                    current.append(val)
+                    counts[val] = counts[val] - 1
+                    permRec(counts, current)
+                    current.pop()
+                    counts[val] = counts[val] + 1
+
+        permRec(counts, list())
+
+        return res
+
+
+Solution().permuteUnique([1,1,2])
