0108349a71b5860a04b8fa42ee60a00bd5a5b777
nadeevkm <32716706+nadeevkm@users.noreply.github.com>
2025-02-17 19:42:29 +0100

    Update 739.java

--- stack/739.java
+++ stack/739.java
@@ -1,1 +1,17 @@
+import java.util.Deque;
+import java.util.LinkedList;
 
+class Solution {
+    public int[] dailyTemperatures(int[] temperatures) {
+        Deque<Integer> st = new LinkedList<>(); // indices
+        int[] ans = new int[temperatures.length];
+        for (int i = temperatures.length - 1; i >= 0; i--){
+            while (!st.isEmpty() && temperatures[st.peek()] <= temperatures[i]){
+                st.pop();
+            }
+            ans[i] = st.isEmpty() ? 0 : (st.peek() - i);
+            st.push(i);
+        }
+        return ans;
+    }
+}
