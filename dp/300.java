77997eec8636f6fefe298c4b317919a96c514651
nadeevkm <32716706+nadeevkm@users.noreply.github.com>
2025-02-17 16:28:41 +0100

    Update 300.java

--- dp/300.java
+++ dp/300.java
@@ -1,1 +1,17 @@
-
+class Solution {
+    public int lengthOfLIS(int[] nums) {
+        int[] mem = new int[nums.length];
+        int overallMax = 0;
+        for (int i = 0; i < nums.length; i++){
+            int max = 0;
+            for (int j = 0; j < i; j++){
+                if (nums[i] > nums[j]){
+                    max = Math.max(max, mem[j]);
+                }
+            }
+            mem[i] = max + 1;
+            overallMax = Math.max(overallMax, mem[i]);
+        }
+        return overallMax;
+    }
+}
