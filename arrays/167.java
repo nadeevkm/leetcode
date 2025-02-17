d9e79cd7c093bb92faf91e4b0b5c37fed24455d0
nadeevkm <32716706+nadeevkm@users.noreply.github.com>
2025-02-17 16:22:17 +0100

    Update 167.java

--- arrays/167.java
+++ arrays/167.java
@@ -1,1 +1,16 @@
-
+class Solution {
+    public int[] twoSum(int[] numbers, int target) {
+        int lp = 0;
+        int rp = numbers.length - 1;
+        while (lp < rp){
+            if (numbers[lp] + numbers[rp] < target) {
+                lp++;
+            } else if (numbers[lp] + numbers[rp] > target){
+                rp--;
+            } else {
+                return new int[]{lp + 1, rp + 1};
+            }
+        }
+        return new int[0];
+    }
+}
