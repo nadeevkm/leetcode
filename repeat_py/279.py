7cdd710c1c35e6ce5eab5bcfb27a85e2d6504222
nadeevkm <32716706+nadeevkm@users.noreply.github.com>
2025-02-17 16:25:37 +0100

    Update 279.py

--- repeat_py/279.py
+++ repeat_py/279.py
@@ -1,1 +1,31 @@
+class Solution:
+    def numSquares(self, n: int) -> int:
+        mem = [0] * (n + 1)
+        sqRoot = 1
+        for i in range(1, n + 1):
+            if i == sqRoot * sqRoot:
+                mem[i] = 1
+                sqRoot += 1
+            else:
+                minVal = i
+                for j in range(1, sqRoot):
+                    minVal = min(minVal, mem[j * j] + mem[i - j * j])
+                mem[i] = minVal
+        return mem[n]
+
+class SolutionConcise:
+    def numSquares(self, n: int) -> int:
+        mem = [0] * (n + 1)
+        for i in range(1, n + 1):
+            minVal = n
+            j = 1
+            while j * j <= i:
+                # min = min(minVal, mem[j * j] + mem[i - j * j]); *** every mem[j*j] is actually '1'
+                minVal = min(minVal, 1 + mem[i - j * j])
+                j += 1
+            mem[i] = minVal
+        return mem[n]
+
+
+Solution().numSquares(2)
 
