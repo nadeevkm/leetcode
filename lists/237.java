e7b842bad9eae69842b37161b767c09335915313
nadeevkm <32716706+nadeevkm@users.noreply.github.com>
2025-02-17 16:13:02 +0100

    Update 237.java

--- lists/237.java
+++ lists/237.java
@@ -1,1 +1,34 @@
+class ListNode {
+    int val;
+    ListNode next;
 
+    ListNode() {
+    }
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
+
+class Solution {
+    public void deleteNode(ListNode node) {
+        while (node.next.next != null){
+            node.val = node.next.val;
+            node = node.next;
+        }
+        node.val = node.next.val;
+        node.next = null;
+    }
+}
+
+class SolutionOptimal { // no need to copy whole list after !!! just copy prev node
+     public void deleteNode(ListNode node) {
+         node.val = node.next.val;
+         node.next = node.next.next;
+    }
+}
