e17b60d9673191dfd2bb3e565f5168ec822e6263
nadeevkm <32716706+nadeevkm@users.noreply.github.com>
2025-02-17 16:23:11 +0100

    Update 172.py

--- repeat_py/172.py
+++ repeat_py/172.py
@@ -1,1 +1,22 @@
+class Solution:
+    def trailingZeroes(self, n: int) -> int:
+        count = 0
+        mem = [0] * (n + 1)
+        for i in range(1, n + 1):
+            # if i % 10 == 0:
+            #     mem[i] = 1 + mem[i // 10]
+            #     count += mem[i]
+            # elif i % 5 == 0:  <- actually we can apply similar logic just to the %5
+            if i % 5 == 0:
+                mem[i] = 1 + mem[i // 5]
+                count += mem[i]
+        return count
 
+
+class SolutionLog5:
+    def trailingZeroes(self, n: int) -> int:
+        count = 0
+        while n != 0:
+            n = n // 5
+            count += n
+        return n
