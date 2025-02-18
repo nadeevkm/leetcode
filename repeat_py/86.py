e8045496cd746f6f674f0d9951ed2dd9fa9e46b9
nadeevkm <32716706+nadeevkm@users.noreply.github.com>
2025-02-18 12:59:21 +0100

    Update 86.py

--- repeat_py/86.py
+++ repeat_py/86.py
@@ -1,1 +1,25 @@
+from typing import Optional
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class Solution:
+    def partition(self, head: Optional[ListNode], x: int) -> Optional[ListNode]:
+        less = dummyL = ListNode()
+        great = dummyG = ListNode()
+        curr = head
+        while curr:
+            if curr.val < x:
+                less.next = curr
+                less = less.next
+            else:
+                great.next = curr
+                great = great.next
+            curr = curr.next
+        less.next = dummyG.next
+        return dummyL.next
+
 
