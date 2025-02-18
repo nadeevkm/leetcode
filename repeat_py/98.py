9a383e6b8188e2f457f60259ecaed77ab642bd31
nadeevkm <32716706+nadeevkm@users.noreply.github.com>
2025-02-18 16:24:26 +0100

    Update 98.py

--- repeat_py/98.py
+++ repeat_py/98.py
@@ -1,1 +1,29 @@
+from typing import Optional
+
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def isValidBST(self, root: Optional[TreeNode]) -> bool:
+        def dfs(root, leftBound, rightBound):
+            if root is None:
+                return True
+            isValid = rightBound > root.val > leftBound
+            if not isValid:
+                return False
+            leftIsValid = dfs(root.left, leftBound, root.val)
+            if not leftIsValid:
+                return False
+            rightIsValid = dfs(root.right, root.val, rightBound)
+            if not rightIsValid:
+                return False
+            return True
+
+        return dfs(root, float('-inf'), float('inf'))
 
