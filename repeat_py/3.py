f38adcb96ffe97cc5c59ac737ba706eb22177246
nadeevkm <32716706+nadeevkm@users.noreply.github.com>
2025-02-18 19:58:17 +0100

    Update 3.py

--- repeat_py/3.py
+++ repeat_py/3.py
@@ -2,12 +2,15 @@ class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
         l = 0
         chars = {}
-        winSize = 0
+        # winSize = 0
         for r, ch in enumerate(s):
             if ch in chars:
                 chars[ch] += 1
             else:
                 chars[ch] = 1
+
+            # or use chars[ch] = chars.get(ch, 0) + 1
+            # or in the beginning chars = defaultdict(int)
 
             if len(chars) < r - l + 1:  # i.e. duplicates found
                 chToPop = s[l]
@@ -16,5 +19,5 @@ class Solution:
                     chars.pop(chToPop)
                 l += 1
 
-            winSize = r - l + 1
-        return winSize
+            # winSize = r - l + 1
+        return r - l + 1  # winSize
