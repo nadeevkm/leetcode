d90a699d20da94fbfdcf3b1055588803d26edcfb
nadeevkm <32716706+nadeevkm@users.noreply.github.com>
2025-02-19 13:36:55 +0100

    Update 55.py

--- repeat_py/55.py
+++ repeat_py/55.py
@@ -1,1 +1,10 @@
-
+class Solution:
+    def canJump(self, nums: List[int]) -> bool:
+        p = 0
+        front = nums[0]
+        while p <= front:
+            front = max(front, p + nums[p])
+            if front >= len(nums) - 1:
+                return True
+            p += 1
+        return False
