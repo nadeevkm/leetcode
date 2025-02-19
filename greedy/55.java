7ec6a418f9db5d02733f890d0e005582de581c38
nadeevkm <32716706+nadeevkm@users.noreply.github.com>
2025-02-19 13:36:40 +0100

    Update 55.java

--- greedy/55.java
+++ greedy/55.java
@@ -1,1 +1,34 @@
+class Solution {
+    public boolean canJump(int[] nums) {  // O(n)
 
+        int reachDistance = 1;
+
+        for (int i = 0; i < nums.length - 1; i++) {
+            reachDistance--;
+            reachDistance = Math.max(reachDistance, nums[i]);
+            if (reachDistance == 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
+
+class SolutionDP {
+    public boolean canJump(int[] nums) {  // DP
+        boolean[] mem = new boolean[nums.length];
+        mem[0] = true;
+
+        for (int i = 1; i < nums.length; i++){
+            for (int j = i - 1; j >= 0; j--){
+                if (mem[j] && j + nums[j] >= i){
+                    mem[i] = true;
+                    break;
+                }
+            }
+        }
+
+        return mem[nums.length - 1];
+    }
+}
