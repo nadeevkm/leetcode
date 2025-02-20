148db5f54fbe195fb5a6b44f3049a3fa91884bb7
nadeevkm <32716706+nadeevkm@users.noreply.github.com>
2025-02-20 13:17:43 +0100

    Update 46.py

--- repeat_py/46.py
+++ repeat_py/46.py
@@ -1,1 +1,20 @@
+class Solution:
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        res = []
+
+        def dfs(perm, used):
+            if len(perm) == len(nums):
+                res.append(perm.copy())
+                return
+
+            for i in range(len(nums)):
+                if i not in used:
+                    perm.append(nums[i])
+                    used.add(i)
+                    dfs(perm, used)
+                    used.remove(i)
+                    perm.pop()
+
+        dfs(list(), set())
+        return res
 
