ad118eec112ee57b5c452bb0f280751627630efb
nadeevkm <32716706+nadeevkm@users.noreply.github.com>
2025-02-20 13:11:10 +0100

    Update 113.py

--- repeat_py/113.py
+++ repeat_py/113.py
@@ -1,1 +1,27 @@
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
 
+class Solution:
+    def pathSum(self, root: Optional[TreeNode], targetSum: int) -> List[List[int]]:
+        res = []
+        def dfs(root, currSum, path):
+            if root is None:
+                return
+
+            currSum += root.val
+            path.append(root.val)
+
+            if root.left is None and root.right is None and currSum == targetSum:
+                res.append(path.copy())
+
+            dfs(root.left, currSum, path)
+            dfs(root.right, currSum, path)
+
+            path.pop()
+
+        dfs(root, 0, list())
+        return res
