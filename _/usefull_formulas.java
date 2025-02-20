ca1df82d6722e679f003d1a20fe7e2324bbfb498
nadeevkm <32716706+nadeevkm@users.noreply.github.com>
2025-02-20 13:12:20 +0100

    Update usefull_formulas.java

--- _/usefull_formulas.java
+++ _/usefull_formulas.java
@@ -1,1 +1,24 @@
+import lombok.val;
 
+class Scratch {
+
+    // Great Common Divisor (Euklid method)
+    public int gcd(int a, int b){
+        if (b == 0){
+            return a;
+        }
+        return gcd(b, a%b);
+    }
+
+    // (Matrix)Diagonals definition/extraction (actually that can be derived either from observations upon matrix cells or from the slop between two points)
+    int diagonalSlash = row + col; //  '/' diag is defined by row + col
+    int diagonalBackSlash = row - col; // '\' diag
+
+
+    //Sudoku inner boxes calculation
+    int boxColStart = 3 * (col / 3); // if we are in r,c cell now; important to put braces ()  here
+    int boxRowStart = 3 * (row / 3);
+    for (int i = 0; i < 9; i++){
+        board[boxRowStart + i / 3][boxColStart + i % 3] // moving within this boxes
+    }
+}
