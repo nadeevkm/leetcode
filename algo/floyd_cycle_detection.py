89ee66f6c0bb7ea0303682e7011b45ed3b3cdd5c
nadeevkm <32716706+nadeevkm@users.noreply.github.com>
2025-02-20 13:13:40 +0100

    Update floyd_cycle_detection.py

--- algo/floyd_cycle_detection.py
+++ algo/floyd_cycle_detection.py
@@ -1,1 +1,26 @@
+from typing import Optional
 
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class CycleDetection:
+    def findCycle(self, head: ListNode) -> Optional[ListNode]:
+        pSlow = head
+        pFast = head
+        hasCycle = False
+        while pFast is not None and pFast.next is not None:
+            pSlow = pSlow.next
+            pFast = pFast.next.next
+            if pSlow == pFast:
+                hasCycle = True
+                break
+        if not hasCycle:
+            return None
+        pSlow = head
+        while pSlow != pFast:
+            pSlow = pSlow.next
+            pFast = pFast.next
+        return pSlow
