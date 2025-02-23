e7f583b2912d3c5cda8a34d90eb0f8edc817a30a
nadeevkm <32716706+nadeevkm@users.noreply.github.com>
2025-02-23 19:41:29 +0100

    Update 424.py

--- repeat_py/424.py
+++ repeat_py/424.py
@@ -1,1 +1,17 @@
+class Solution:
+    def characterReplacement(self, s: str, k: int) -> int:
+        l = 0
+        chars = {}
+        mostFrChFreq = 0
+        for r in range(len(s)):
+            chToAdd = s[r]
+            chars[chToAdd] = chars.get(chToAdd, 0) + 1
+            chFr = chars[chToAdd]
+            mostFrChFreq = max(mostFrChFreq, chFr)
 
+            if r - l + 1 > mostFrChFreq + k:  # size of window is > than k + freq of most freq char => wis is not valid => need to schrink
+                chToPop = s[l]
+                chars[chToPop] -= 1
+                l += 1
+
+        return r - l + 1
