aa9daebd0ea4339a8e9cf859239d7ff0c8ecc939
nadeevkm <32716706+nadeevkm@users.noreply.github.com>
2025-08-20 13:30:28 +0200

    Create 209.java

---
+++ sliding-window/209.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int minSubArrayLen(int target, int[] nums) {
+        int l = 0;
+        int minLen = Integer.MAX_VALUE;
+        int sum = 0;
+        for (int r = 0; r < nums.length; r++){
+            sum += nums[r];
+
+            while(sum >= target){
+                minLen = Math.min(minLen, r - l + 1);
+                sum -= nums[l];
+                l++;
+            }
+        }
+        return minLen == Integer.MAX_VALUE ? 0 : minLen;
+    }
+}
